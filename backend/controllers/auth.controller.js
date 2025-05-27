157e83efb03c19f33b9933c09670b1eff04b9477
inornnut01 <inornnut01@gmail.com>
2025-05-27 16:31:11 +0700

    setup backend start project and User model

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  res.send("Hello World Signup");
+};
+
+export const login = (req, res) => {
+  res.send("Hello World Login");
+};
+
+export const logout = (req, res) => {
+  res.send("Hello World Logout");
+};
