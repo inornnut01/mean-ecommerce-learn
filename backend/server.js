167dfa0085bca29a698c9bdfb93bac1d35d10cbd
inornnut01 <inornnut01@gmail.com>
2025-06-06 08:21:35 +0700

    Product Routes - add protectRoute, adminRoute and getAllProduct. also product model

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 import connectDB from "./lib/db.js";
+
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -14,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
