ead23546d8b8b7605e8f91573513e9cad47a50a7
inornnut01 <inornnut01@gmail.com>
2025-06-09 08:54:22 +0700

    Add coupon functionality: create coupon model, controller, and routes. Integrate coupon routes into the server and implement coupon validation and retrieval logic.

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,9 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
-import connectDB from "./lib/db.js";
+import couponRoutes from "./routes/coupon.route.js";
 
+import connectDB from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
